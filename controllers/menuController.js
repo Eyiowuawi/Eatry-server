a9473d0b9077dc6cf14dd151bdf9df9235a13110
Toluwalase Adeosun <temilolaadeosun15@gmail.com>
2024-06-22 00:36:20 +0100

    test deployment

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -2,8 +2,8 @@ const MenuItem = require("../models/menuItemsModel");
 
 exports.getMenu = async (req, res) => {
   try {
-    // const menuItems = await MenuItem.find();
-    res.json({name: "toluwalase"});
+    const menuItems = await MenuItem.find();
+    res.json(menuItems);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
