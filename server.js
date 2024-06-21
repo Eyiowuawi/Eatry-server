4b3d5bb5c97da8030f57c114a22585d2f80e45a7
Toluwalase Adeosun <temilolaadeosun15@gmail.com>
2024-06-21 09:44:01 +0100

    database connection

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
+// connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/menu", require("./routes/menuRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
